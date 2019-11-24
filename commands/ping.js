65ad4b7d65df2c805bb58fc85d3385d52f0df00b
AwesomenessZ <isaac@daebelliehn.tk>
2019-11-24 17:58:33 -0500

    Made ping an embed

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,18 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message, args) {
-		message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
+			message.channel.send({embed: {
+  		color: message.guild.me.displayColor,
+			timestamp: new Date(),
+			footer: {
+				text: `Requested by ${message.member.displayName}`
+						},
+						fields: [
+							{
+								name: 'Discord Ping:',
+								value: (new Date().getTime() - message.createdTimestamp + " ms"),
+							}]
+
+}});
 	},
 };
