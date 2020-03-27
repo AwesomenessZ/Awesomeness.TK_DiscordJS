ee88778fc0af1161b5a32f50bf6b1c5d771b361c
AwesomenessZ <isaac@daebelliehn.tk>
2020-03-27 14:42:19 -0400

    Added basic support for AwesomenessZ archives

--- bot.js
+++ bot.js
@@ -85,10 +85,12 @@ client.on('message', message => {
 	//Support for MoleRatArchives
 		if(message.channel.id == 582073429809692682){
 			var loadchannel = client.channels.get("654465946395607100");
+			var loadchannel2 = client.channels.get("693165889914405004");
 			if(message.attachments.size > 0){
 				var attached = (message.attachments).array();
 				var content = ": " + message.content
 				for(var i = 0; i < attached.length;i++){
+					//MoleRatArchives`
 				loadchannel.send({embed: {
 					color: message.guild.me.displayColor,
 					timestamp: new Date(),
@@ -112,6 +114,30 @@ client.on('message', message => {
 				],
 
 			}})
+			//AwesomenesZ Archives
+			loadchannel2.send({embed: {
+				color: message.guild.me.displayColor,
+				timestamp: new Date(),
+				title: `New Image in Art Channel`,
+				footer: {
+					text: `Posted by ${message.member.displayName}`,
+					icon_url: message.author.avatarURL
+				},
+				image: {
+					url:attached[i].url
+				},
+				fields: [
+					{
+						name: 'File Details:',
+						value: `${attached[i].filename}, ${Math.round((attached[i].filesize)/1040)}kb`,
+					},
+					{
+						name: 'Included Message:',
+						value: `${content}`,
+					}
+			],
+
+		}})
 				}
 			}
 			if(message.attachments.size == 0){
