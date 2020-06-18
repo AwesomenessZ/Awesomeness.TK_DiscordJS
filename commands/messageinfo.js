d089c93b95bd7f32ea0e706da81923e29343d210
AwesomenessZ <isaac@daebelliehn.tk>
2020-06-18 14:52:06 -0400

    More message info!

--- commands/messageinfo.js
+++ commands/messageinfo.js
@@ -5,6 +5,6 @@ module.exports = {
 	//Code to be executed
 	execute(message) {
 		//Sends varius infromation about the message that invoked this command to be run
-		message.channel.send(`Channel id:\n${message.channel}\n${message.channel.id}\nMessage ID:\n${message.id}\nMessage.user.avatar: \nhttps://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`);
+		message.channel.send(`Channel id:\n${message.channel}\n${message.channel.id}\nMessage ID:\n${message.id}\nMessage.user.avatar: \nhttps://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png \nMessage Content:\n${message.content.split(" ")} \n Message.Author: \n ${message.author}`);
 	},
 };
