d99b0d603fa8875bb3d12f8caf00869aeda46416
AwesomenessZ <isaac@daebelliehn.tk>
2021-03-02 13:11:56 -0800

    Made emojiletters output copyable

--- commands/emojiletters.js
+++ commands/emojiletters.js
@@ -8,7 +8,6 @@ module.exports = {
   //code to be executed
   execute(message, args, displayColor) {
     var msg = args.join(" ");
-    var og = msg;
     msg = msg.toLowerCase();
     msg = msg.split("");
     var i;
@@ -18,6 +17,7 @@ module.exports = {
       }
     }
     msg = msg.join("");
+    message.channel.send(msg);
     message.channel.send({
       embed: {
         color: displayColor,
@@ -25,17 +25,7 @@ module.exports = {
         footer: {
           text: `Requested by ${message.author.username}`,
           icon_url: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.png`
-        },
-        fields: [
-          {
-            name: "Original Message:",
-            value: og
-          },
-          {
-            name: "Converted Message:",
-            value: msg
-          }
-        ]
+        }
       }
     });
   }
